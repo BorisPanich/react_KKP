45e940b097268eade907702c3be814608062f123
BorisPanich <panich2303@gmail.com>
2021-07-18 06:35:41 +0300

    typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import './App.css';
 import Accordion from './components/Accordion';
-import Rating from './components/Raiting';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div>
-      <PageTitle title={"Project KKP"} />
+      {/* <PageTitle title={"Project KKP"} />
       <PageTitle title={"title 2"} />
-      Article 1 Raiting
-      <Rating value={2} />
-      <Accordion title={"Menu Accordion 1"} />
-      <Accordion title={"Menu Accordion 2"} />
-      Article 2 Raiting
+      Article 1 Rating
+      <Rating value={2} /> */}
+      <Accordion titleValue={"Menu Accordion"} colapsed={true} />
+      <Accordion titleValue={"Users Accordion"} colapsed={false} />
+      {/* Article 2 Rating
       <Rating value={0} />
       <Rating value={1} />
       <Rating value={2} />
       <Rating value={3} />
       <Rating value={4} />
       <Rating value={5} />
-      <Rating value={6} />
+      <Rating value={6} /> */}
     </div>
   );
 }
 
-function PageTitle(props: any) {
+type PageTitlePropsTitle = {
+  title: string
+}
+
+function PageTitle(props: PageTitlePropsTitle) {
   return <h1>{props.title}</h1>
 }
 
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 
-function Accordion(props: any) {
+type AccordionPropsType = {
+  titleValue: string
+  colapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+
+  if (props.colapsed === true) {
     return <div>
-      <AccordionTitle title={props.title} />
+      <AccordionTitle title={props.titleValue} />
+    </div>
+  } else {
+    return <div>
+      <AccordionTitle title={props.titleValue} />
       <AccordionBody />
     </div>
   }
-  
-  function AccordionTitle(props: any) {
-    return <h2>{props.title}</h2>
-  }
-  
-  function AccordionBody() {
-    return (
-      <div>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </div>
-    )
-  }
+}
 
-  export default Accordion;
+type AccordionTitlePropsType = {
+  title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+  return <h2>{props.title}</h2>
+}
+
+type AccordionBodyPropsType = {
+
+}
+
+function AccordionBody(props: AccordionBodyPropsType) {
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default Accordion;
